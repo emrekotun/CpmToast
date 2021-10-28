c2dc7064f0bee6d56cebe67993bbd48f66645e78
Emre KOTUN <emre.kotun@palamutgroup.com.tr>
2021-10-28 17:37:34 +0300

    upgrade

--- app/src/main/java/com/emrekotun/cpmtoast/MainActivity.kt
+++ app/src/main/java/com/emrekotun/cpmtoast/MainActivity.kt
@@ -2,6 +2,7 @@ package com.emrekotun.cpmtoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.emrekotun.toast.CpmToast
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,12 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       /* CpmToast.createColorToast(
+     CpmToast.createColorToast(
             this,
-            message,
-            CpmToast.TOAST_ERROR,
+            "Success !",
+            CpmToast.TOAST_SUCCESS,
             CpmToast.GRAVITY_TOP,
             CpmToast.LONG_DURATION
-        )*/
+        )
     }
 }
